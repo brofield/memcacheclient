48f2b45cbf0fd5924916a61059fc355a2c680ce8
brofield <brofield@7630c5ff-bc4e-0410-8d6e-a582cec6224d>
2008-09-30 13:08:03 +0000

    release 1.2

---
+++ tags/1.2/package.cmd
@@ -0,0 +1,32 @@
+set VERSION=1.2
+
+set SEVENZIP="C:\Program Files\7-Zip\7z.exe"
+
+FOR /F "tokens=*" %%G IN ('DIR /AD /B /S lib*') DO (
+    DEL /S /Q "%%G"
+    RD "%%G"
+)
+FOR /F "tokens=*" %%G IN ('DIR /AD /B /S _upgrade*') DO (
+    DEL /S /Q "%%G"
+    RD "%%G"
+)
+FOR /F "tokens=*" %%G IN ('DIR /AD /B /S Debug*') DO (
+    DEL /S /Q "%%G"
+    RD "%%G"
+)
+FOR /F "tokens=*" %%G IN ('DIR /AD /B /S Release*') DO (
+    DEL /S /Q "%%G"
+    RD "%%G"
+)
+DEL /Q "memcacheclient.ncb"
+ATTRIB -H "memcacheclient.suo*"
+DEL /Q "memcacheclient.suo*" 
+DEL /Q "memcacheclient.opt"
+DEL /Q "*.sln.old" "*.vcproj.*.user" "*.vcproj.*.old" "upgradelog.xml"
+START "Generate documentation" /WAIT memcacheclient.doxy
+cd ..
+del memcacheclient-%VERSION%.zip
+%SEVENZIP% a -tzip -r- -x!memcacheclient\.svn memcacheclient-%VERSION%.zip memcacheclient\*
+del memcacheclient-doc.zip
+%SEVENZIP% a -tzip -r memcacheclient-doc.zip memcacheclient-doc\*
+cd memcacheclient
